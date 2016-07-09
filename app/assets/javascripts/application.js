58872d1ad3d8a3d2a8e04a3be7c5f93b42b8aaa3
Волощук Александр <ai_voloshuk@mail.ru>
2016-07-09 20:57:43 +0300

    add pagination view, add  index page view,add footer view, add header view, add bootstrap, add jquery, add font-awesome,add salvattore,add hotels list view, add hotelstop(carousel) view, fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
 //= require_tree .
+//=require jquery-3.0.0.min
+//=require jquery.easing.1.3
+//=require bootstrap
